5c834e91f8105f9e8aeba7370d467871cc57119a
nhatminhbeo <tqnmb437@gmail.com>
2017-02-21 17:15:57 -0800

    added /logic(routes)/data.js to serve pure data such as list of classes, list of majors, list of study habits

---
+++ logic/data.js
@@ -0,0 +1,55 @@
+// ===========================================================================
+// File: /logic/data.js
+// Description: Export API functions that handle requests at /api/data/*
+// Author:
+// Last updated: Feb 12 2017
+// ===========================================================================
+
+
+// ===============================================================================================================================================
+//    Path                    |   Method    |   Purpose / Brief Description
+// ===============================================================================================================================================
+//                                   Data (major, class)
+// ===============================================================================================================================================
+//    /api/data/class         |   GET       |   Get list of classes
+//    /api/data/major         |   GET       |   Get list of majors
+//    /api/data/habit         |   GET       |   Get list of habits
+// ===============================================================================================================================================
+
+// ================================================================================
+//  Function: getDataClass
+//  REST: GET:/api/data/class
+//  Description: Return list of all possible classes
+//  Expected input (req.body):
+//  Expected output (res):
+//  Author: 
+// ================================================================================
+exports.getDataClass = function (req, res) {
+
+};
+
+
+// ================================================================================
+//  Function: getDataMajor
+//  REST: GET:/api/data/major
+//  Description: Return list of all possible majors
+//  Expected input (req.body):
+//  Expected output (res):
+//  Author: 
+// ================================================================================
+exports.getDataMajor = function (req, res) {
+
+};
+
+
+// ================================================================================
+//  Function: getDataHabit
+//  REST: GET:/api/data/habit
+//  Description: Return list of all possible habits
+//  Expected input (req.body):
+//  Expected output (res):
+//  Author: 
+// ================================================================================
+exports.getDataHabit = function (req, res) {
+
+};
