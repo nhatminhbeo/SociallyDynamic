9f03462227ba5ce56d074f362637239b842de18a
nhatminhbeo <tqnmb437@gmail.com>
2017-02-28 15:50:56 -0800

    merging all

--- logic/general.js
+++
@@ -1,7 +0,0 @@
-// ===========================================================================
-// File: /logic/general.js
-// Description: Export general purpose api functions
-// Author:
-// Last updated: Feb 12 2017
-// ===========================================================================
-module.exports.User = mongoose.model();
