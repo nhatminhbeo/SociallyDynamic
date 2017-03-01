15cdfc25518b2e07e93ba00a3a0e42600ba62bb5
Shiva Kannan <s1kannan@ucsd.edu>
2017-03-01 01:45:56 -0800

    Brendon Chen Profile!

--- public/controller/profileController.js
+++ public/controller/profileController.js
@@ -1,17 +1,20 @@
 app.controller('profileController', ['$scope', 'authService', '$location','$http', function($scope, authService, $location,
 $http) {
+
     $scope.logout = function() {
         // log user out
         authService.Auth.$signOut().then(function(){
             $location.path('/');
         });
     }
-    
-    $scope.name = function() {
-        // Get the name
-    }
 
     $scope.editName = function() {
         // Edit the name
+    };
+
+    var getProfile = function() {
+        $scope.name = "Name";
     }
+
+    getProfile();    
 }]);
