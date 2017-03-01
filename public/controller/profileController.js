a96b6bbb91b2f03554dfa0b4e978729f9ac57a35
Brendon Chen <brc019@ucsd.edu>
2017-03-01 14:38:10 -0800

    Added more code to profile

--- public/controller/profileController.js
+++ public/controller/profileController.js
@@ -1,6 +1,8 @@
 app.controller('profileController', ['$scope', 'authService', '$location','$http', function($scope, authService, $location,
 $http) {
 
+    var DEBUG = true;
+
     $scope.logout = function() {
         // log user out
         authService.Auth.$signOut().then(function(){
@@ -8,13 +10,53 @@ $http) {
         });
     }
 
-    $scope.editName = function() {
-        // Edit the name
+    $scope.modifyClassList = function() {
+
+        // Modify User Class List
+        if (DEBUG) {
+            console.log("modifyClassList() called")
+        }
+    }
+
+    $scope.modifyUserBio = function() {
+
+        // Modify User Bio
+        if (DEBUG) {
+            console.log("modifyUserBio() called");
+        }
+
+    };
+
+    $scope.modifyStudyHabit = function() {
+
+        // Modify User Bio
+        if (DEBUG) {
+            console.log("modifyStudyHabit() called");
+        }
+
+    };
+
+    $scope.deleteUserProfile = function() {
+
+        // Delete User Profile
+        if (DEBUG) {
+            console.log("deleteUserProfile() called");
+        }
+
     };
 
     var getProfile = function() {
+
+        if (DEBUG) {
+            console.log("getProfile() called");
+        }
+
         $scope.name = "Name";
+        $scope.classList = "Class List";
+        $scope.userBio = "User Bio";
+        $scope.studyHabit = "Study Habit";
     }
 
-    getProfile();    
+    getProfile();
+    $scope.isSelf = true;
 }]);
