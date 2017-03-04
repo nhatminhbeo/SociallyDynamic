414c7a2c5f42dc9edf086769d4d11cdef2cd9bb0
eyvonnehu <eyvonnehu@gmail.com>
2017-03-04 15:14:33 -0800

    conversation updates

--- public/controller/conversationController.js
+++ public/controller/conversationController.js
@@ -1,10 +1,18 @@
 app.controller('conversationController', ['$scope', 'authService', '$location','$http', function($scope, authService, $location,
  $http) {
     $scope.conversationController = "conversationController";
+    $scope.message="";
     $scope.logout = function() {
         // log user out
         authService.Auth.$signOut().then(function(){
             $location.path('/');
         });
-    }
+}
+     $scope.sendMessage = function() {
+     	$scope.message = "";
+     // Modify User Class List
+     if (true) {
+        console.log("sendMessage() called")
+     }
+    }    
 }]);
