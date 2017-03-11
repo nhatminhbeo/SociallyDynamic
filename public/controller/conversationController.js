f6ac8e04185f5a8335a64d67183a7d4044cc26b7
eyvonnehu <eyvonnehu@gmail.com>
2017-03-10 19:03:03 -0800

    chat edits

--- public/controller/conversationController.js
+++ public/controller/conversationController.js
@@ -2,12 +2,14 @@ app.controller('conversationController', ['$scope', 'authService', '$location','
  $http, currentUser) {
     $scope.conversationController = "conversationController";
     $scope.message="";
+    $scope.messages = [];
     $scope.logout = function() {
         // log user out
         authService.Auth.$signOut().then(function(){
             $location.path('/');
         });
 }
+
      $scope.sendMessage = function() {
      	$scope.message = "";
      // Modify User Class List
@@ -15,4 +17,19 @@ app.controller('conversationController', ['$scope', 'authService', '$location','
         console.log("sendMessage() called")
      }
     }    
+console.log($scope.messages);
+
+    //take user inputted message and display on chat screen
+    $scope.userSendMessage = function(){
+    	$scope.message = $scope.message.trim();
+    	if($scope.message=="") {
+    		return;
+    	}
+    	$scope.messages.push($scope.message);
+    	$scope.message = "";
+    }i
+
+
+
+
 }]);
