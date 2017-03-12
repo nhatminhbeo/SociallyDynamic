a732d4b0a25a70fde93b23b41e9c21d4ebd6ed52
Brendon Chen <brc019@ucsd.edu>
2017-03-11 18:32:20 -0800

    Fixed some styling issues

--- public/controller/profileController.js
+++ public/controller/profileController.js
@@ -1,12 +1,18 @@
 app.controller('profileController', ['$scope', 'authService', '$location','$http', 'currentUser', '$routeParams', function($scope, authService, $location,
 $http, currentUser, $routeParams) {
 
-    if (!currentUser) {
-        $location.path('/');
-    }
-
     var DEBUG = true;
     var DUMMY_TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+    if (!currentUser) {
+
+        if (DEBUG) {
+            console.log("Not logged in; redirecting");
+        }
+
+        $location.path('/');
+    }
+    else { // <-- Do not delete this
 
     // Limit for search
     $scope.quantity = 5;
@@ -588,4 +594,5 @@ $http, currentUser, $routeParams) {
     getClasses();
     getMajors();
     getStudyHabits();
+    } // <-- Do not delete this
 }]);
