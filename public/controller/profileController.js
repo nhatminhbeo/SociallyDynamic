520c19f7f77bcf8bdc2b3fe734458f29b5dc0a00
Brendon Chen <brc019@ucsd.edu>
2017-03-12 15:58:09 -0700

    Turned DEBUG mode off

--- public/controller/profileController.js
+++ public/controller/profileController.js
@@ -1,7 +1,7 @@
 app.controller('profileController', ['$scope', 'authService', '$location','$http', 'currentUser', '$routeParams', function($scope, authService, $location,
 $http, currentUser, $routeParams) {
 
-    var DEBUG = true;
+    var DEBUG = false;
     var DUMMY_TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
     if (!currentUser) {
